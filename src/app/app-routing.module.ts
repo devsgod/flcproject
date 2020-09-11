52a8f40493888999566b82db8b881fa764631320
Devs God <developer9517@yandex.com>
2020-09-11 11:22:05 +0800

    flc911

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaymentsComponent } from './pages/payments/payments.component';
+import { FollowupComponent } from './pages/followup/followup.component';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { CreateissueComponent } from './pages/createissue/createissue.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    'path':'payments',
+    'component': PaymentsComponent
+  },
+  {
+    'path':'followup',
+    'component': FollowupComponent
+  },
+  {
+    'path':'collection',
+    'component': CollectionComponent
+  },
+  {
+    'path':'createissue',
+    'component': CreateissueComponent
+  },
+  {
+    'path':'login',
+    'component': LoginComponent
+  },
+  {
+    'path':'register',
+    'component': RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { PaymentsComponent } from './pages/payments/payments.component';
+import { FollowupComponent } from './pages/followup/followup.component';
+import { CreateissueComponent } from './pages/createissue/createissue.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent,
+    PaymentsComponent,
+    FollowupComponent,
+    CreateissueComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
